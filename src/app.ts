2b91ae1777b0a65a6fe7e58c2e54d63a0bcdce63
Paramr24c <nayanparmar2190@gmail.com>
2025-06-30 17:07:17 +0530

    remove v2

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { customResponseMiddleware } from './common/middleware/response_middleware';
 import v1Routes from "./v1/routes/_routes";
-import v2Routes from "./v2/routes/_routes";
+// import v2Routes from "./v2/routes/_routes";
 import { errorHandler } from './common/middleware/error_handler';
 
 const app: Application = express();
@@ -31,7 +31,7 @@ app.use("/uploads", express.static("uploads"));
 
 // Versioned Routing
 app.use("/api/v1", v1Routes);
-app.use("/api/v2", v2Routes);
+// app.use("/api/v2", v2Routes);
 
 
 app.use(errorHandler);
